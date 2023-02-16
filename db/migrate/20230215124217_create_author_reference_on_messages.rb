6d5f4340da5166d718ab0c38d30047cb308e3fe4
Thomas Hareau <thomas.hareau@doctolib.com>
2023-02-16 12:07:32 +0100

    Explicit migration

--- db/migrate/20230215124217_create_author_reference_on_messages.rb
+++ db/migrate/20230215124217_create_author_reference_on_messages.rb
@@ -1,5 +1,9 @@
 class CreateAuthorReferenceOnMessages < ActiveRecord::Migration[7.0]
   def change
-    add_reference :messages, :author, foreign_key: true
+    add_column :messages, :author_id, :bigint
+
+    add_index :messages, :author_id
+
+    add_foreign_key :messages, :authors
   end
 end
