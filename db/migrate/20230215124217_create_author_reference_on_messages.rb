0296a97649cfa196a99fd35c1a2ad396b8f3c8c1
Thomas Hareau <thomas.hareau@doctolib.com>
2023-02-21 16:56:06 +0100

    Add statement timeout as a seatbell

--- db/migrate/20230215124217_create_author_reference_on_messages.rb
+++ db/migrate/20230215124217_create_author_reference_on_messages.rb
@@ -1,9 +1,14 @@
 class CreateAuthorReferenceOnMessages < ActiveRecord::Migration[7.0]
   def change
+    old_value = query_value("SHOW statement_timeout")
+    execute "SET statement_timeout TO '5s'"
+
     add_column :messages, :author_id, :bigint
 
     add_index :messages, :author_id
 
     add_foreign_key :messages, :authors
+
+    execute "SET statement_timeout TO #{quote(old_value + "s")}"
   end
 end
