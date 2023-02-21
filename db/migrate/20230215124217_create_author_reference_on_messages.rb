160da2c22b4b14ea300f229fa22f63923db55bf2
Thomas Hareau <thomas.hareau@doctolib.com>
2023-02-21 14:45:01 +0100

    Make add_index safe

--- db/migrate/20230215124217_create_author_reference_on_messages.rb
+++ db/migrate/20230215124217_create_author_reference_on_messages.rb
@@ -5,7 +5,11 @@ class CreateAuthorReferenceOnMessages < ActiveRecord::Migration[7.0]
 
     add_column :messages, :author_id, :bigint
 
-    add_index :messages, :author_id
+    execute "SET statement_timeout TO '0s'"
+
+    add_index :messages, :author_id, algorithm: :concurrently
+
+    execute "SET statement_timeout TO '5s'"
 
     add_foreign_key :messages, :authors
 
